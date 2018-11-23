6227bc240f645de8ec6f28ba8aba33462a818c5a
cafealpha <cyanine@naver.com>
2018-11-23 10:04:39 +0900

    가속도센서를 표시할 뷰 생성

--- app/src/main/java/com/example/hi/tiltsensor/MainActivity.kt
+++ app/src/main/java/com/example/hi/tiltsensor/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.example.hi.tiltsensor
 
 import android.content.Context
+import android.content.pm.ActivityInfo
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() , SensorEventListener {
+    private lateinit var tiltView: TiltView
+
+
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented") //To chan
+        setContentView(tiltView)ge body of created functions use File | Settings | File Templates.
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        //센서값이 변경되면 호출됨
+        // values[0] : x축값 : 위로 기울이면 -10~0, 아래로 기울이면 0-10
+        // values[1] : y축값 : 왼쪽 -10-0, 오른쪽 0-10
+        // values[2] : z축값 : 미사용
+        event?.let{
+            Log.d("MainActivity", "onSensorChanged: x : " +
+                    "${event.values[0]}, y : ${event.values[1]}, z: ${event.values[2]}")
+        }
     }
 
     private val sensorManager by lazy {
@@ -22,8 +36,15 @@ class MainActivity : AppCompatActivity() , SensorEventListener {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        //화면 항상 켜지
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        //가로모드로 고정
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        tiltView = TiltView(this)
+
+        //setContentView(R.layout.activity_main)
     }
 
     override fun onResume() {
---
+++ app/src/main/java/com/example/hi/tiltsensor/TiltView.kt
@@ -0,0 +1,11 @@
+package com.example.hi.tiltsensor
+
+import android.content.Context
+import android.view.View
+
+class TiltView(context: Context?) : View(context) {
+
+
+
+
+}
